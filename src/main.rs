94d542efa95dba65ae0458ea5ae5087ee9e17a8e
Ernest C. Suarez <ernestcsuarez@gmail.com>
2021-10-29 20:24:43 -0400

    Add commandline help

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,33 @@ mod lib;
 use lib::{run, Config};
 use std::{env, process};
 
+fn show_help() {
+    println!("{} {}", env::args().nth(0).expect("minigrep"), "-Like grep");
+    // Arguments
+    // Optional
+    println!();
+    println!("{}", "Optional Argumets:");
+    println!(" {}    {}", "--help", "-Show help");
+    // Required
+    println!();
+    println!("{}", "Required Arguments:");
+    println!(" {}    {}", "text", "-Text to search");
+    println!(" {}    {}", "file", "-File to search");
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // Show  the help if the first argument is --help
+    if args.len() == 2 {
+        if args[1] == "--help" {
+            show_help();
+        } 
+        process::exit(0);
+    }
+
     let config = Config::new(&args).unwrap_or_else(|err| {
-        eprintln!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}. {}", err, "Use --help for help");
         process::exit(1);
     });
 
